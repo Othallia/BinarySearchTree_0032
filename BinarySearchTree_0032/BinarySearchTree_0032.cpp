dc3c967b13ebb9a1347cdf2e8f6c50b244f095cd
Othallia <vianiputriocta6@gmail.com>
2024-06-13 18:42:30 +0700

    membuat anak kiri

--- BinarySearchTree_0032/BinarySearchTree_0032.cpp
+++ BinarySearchTree_0032/BinarySearchTree_0032.cpp
@@ -32,29 +32,4 @@ public:
 		Node* newNode = +new Node(element, nullptr, nullptr); //allocate memory for the new node
 		newNode->info = element; //asign value to the data field of the new data
 		newNode->leftchild = nullptr; //make the left child of the new node point to null
-		newNode->rightchild = nullptr; //make the right child of the new data point to null
-
-		Node* parent = nullptr;
-		Node* currentNode = nullptr;
-		search(element, parent, currentNode); //locate the node which be the parent of the node to be inserted
-
-
-		if (parent == nullptr) //if the parent is NULL (Tree is empty)
-		{
-			ROOT = newNode; //mark the new node as ROOT
-			return; //exit
-		}
-
-		if (element < parent->info) // If the value in the data field of the new node is less than that of the
-		{
-			parent->leftchild = newNode; // Make the left child of the parent point to the new node
-		}
-		else if (element > parent->info) // If the value in the data field of the new node is greater than that of the
-		{
-			parent->rightchild = newNode; // Make the right child of the parent point to the new node
-		}
-	}
-
-};
-
-
+		
