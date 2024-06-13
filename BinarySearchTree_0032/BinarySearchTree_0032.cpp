aa510ecc3b648674a382eeeca772fc420ce9bdf9
Othallia <vianiputriocta6@gmail.com>
2024-06-13 18:42:57 +0700

    membuat +anak kanan

--- BinarySearchTree_0032/BinarySearchTree_0032.cpp
+++ BinarySearchTree_0032/BinarySearchTree_0032.cpp
@@ -32,4 +32,6 @@ public:
 		Node* newNode = +new Node(element, nullptr, nullptr); //allocate memory for the new node
 		newNode->info = element; //asign value to the data field of the new data
 		newNode->leftchild = nullptr; //make the left child of the new node point to null
+		newNode->rightchild = nullptr; //make the right child of the new data point to null
+
 		
